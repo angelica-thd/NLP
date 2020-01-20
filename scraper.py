a14283cff83e46c73a92b0603f9fd40575c1beb0
angelica-thd <34653549+angelica-thd@users.noreply.github.com>
2020-01-20 19:16:03 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,12 +3,12 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 import argparse
 import requests
-import json
+import sys
 import re
 
 SITE_URL = 'https://old.reddit.com/'
 REQUEST_AGENT = 'Mozilla/5.0 Chrome/47.0.2526.106 Safari/537.36'
-userBtn = False
+
 
 def createSoup(url):
     return BeautifulSoup(requests.get(url, headers={'User-Agent':REQUEST_AGENT}).text, 'lxml')
@@ -24,7 +24,7 @@ def getSearchResults(searchUrl):
         else:
             return posts
 
-def parseComments(commentsUrl):
+def parseComments(commentsUrl,user):
     commentTree = {}
     commentsPage = createSoup(commentsUrl)
     commentsDiv = commentsPage.find('div', {'class':'sitetable nestedlisting'})
@@ -39,14 +39,24 @@ def parseComments(commentsUrl):
         parent = comment.find('a', {'data-event-action':'parent'})
         parentId = parent['href'][1:] if parent != None else '       '
         parentId = '       ' if parentId == commentId else parentId
+        commentTree[commentId] = {'author':author, 'reply-to':parentId, 'text':content, 'num-replies':numReplies}
+        return commentTree
         #print(commentId, 'reply-to:', parentId, 'num-replies:', numReplies, content[:63])
-        if userBtn:
-            commentTree[commentId] = {'author':user, 'reply-to':parentId, 'text':content, 'num-replies':numReplies}
+'''  commented cause it doesnt work as expected, will fix later
+            if len(user)!=0:
+            commentTree1 = {}
+            commentTree1[commentId] =  {'author':author, 'reply-to':parentId, 'text':content, 'num-replies':numReplies}
+            if user in commentTree1.items():
+                author = user
+                commentTree[commentId] = {'author':author, 'reply-to':parentId, 'text':content, 'num-replies':numReplies}
+            else:
+               break
+               print("No post from user:"+user+" in this reddit forum...")
         else:
-            commentTree[commentId] = {'author':author, 'reply-to':parentId, 'text':content, 'num-replies':numReplies}
-    return commentTree
+            commentTree[commentId] = {'author':author, 'reply-to':parentId, 'text':content, 'num-replies':numReplies}'''
+    
 
-def parsePost(post, results):
+def parsePost(post, results, user):
     title = post.find('a', {'class':'search-title'}).text
     author = post.find('a', {'class':'author'}).text
     subreddit = post.find('a', {'class':'search-subreddit-link'}).text
@@ -54,42 +64,26 @@ def parsePost(post, results):
     url = commentsTag['href']
     numComments = int(re.match(r'\d+', commentsTag.text).group(0))
     print("\n" + ":", numComments, author, subreddit, title)
-    commentTree = {} if numComments == 0 else parseComments(url)
+    commentTree = {} if numComments == 0 else parseComments(url,user)
     results.append({'title':title, 'author':author, 'subreddit':subreddit, 'comments':commentTree})
 
-'''
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--keyword', type=str, help='keyword to search')
-    parser.add_argument('--subreddit', type=str, help='optional subreddit restriction')
-  
-    args = parser.parse_args()
-    if args.keyword == None:
-        print('ERROR: No search keyword specified.')
-        exit()
-    if args.subreddit == None:
-        searchUrl = SITE_URL + 'search?q="' + args.keyword + '"'
-    else:
-        searchUrl = SITE_URL + 'r/' + args.subreddit + '/search?q="' + args.keyword + '"&restrict_sr=on'
-   '''
-def returnResults(searchUrl,keyword,subreddit):
-    
+
+def returnResults(searchUrl,keyword,subreddit,user):
+    sys.setrecursionlimit(10000)
     product = {}
   #  print('Search URL:', searchUrl)
     posts = getSearchResults(searchUrl)
-   # print('Started scraping', len(posts), 'posts.')
-#    keyword = args.keyword.replace(' ', '-')
     product[keyword] = {}
- #   product[keyword]['subreddit'] = 'all' if args.subreddit == None else args.subreddit
     product[keyword]['subreddit'] = 'all' if subreddit == None else subreddit
     results = Manager().list()
     jobs = []
     for post in posts:
-        job = Process(target=parsePost, args=(post, results))
+        job = Process(target=parsePost, args=(post, results, user))
         jobs.append(job)
         job.start()
     for job in jobs:
         job.join()
     product[keyword]['posts'] = list(results)
     postList = product[keyword]['posts']
+    #print(str(len(posts))+"posts")
     return postList    
